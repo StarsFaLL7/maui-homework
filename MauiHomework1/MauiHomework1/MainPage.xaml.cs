cd0eebe74b124001a7f5ef8fd1976ba6331b8786
StarsFaLL7 <starsfall-ru@yandex.ru>
2025-10-08 13:14:17 +0500

    [HW] Выполнено первое домашнее задание по созданию простой анкеты.

---
+++ MauiHomework1/MauiHomework1/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace MauiHomework1;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnSubmitClicked(object sender, EventArgs e)
+    {
+        NameEntry.Text = "";
+        AgeSlider.Value = 20;
+        GenderMaleRadioButton.IsChecked = true;
+        AgreementCheckBox.IsChecked = false;
+        PhoneEntry.Text = "";
+    }
+
+    private void AgreementCheckBox_OnCheckedChanged(object? sender, CheckedChangedEventArgs e)
+    {
+        SubmitBtn.IsEnabled = e.Value;
+    }
+
+    private void PhoneEntry_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        if (sender is not Entry entry)
+        {
+            return;
+        }
+        var digitsOnly = new string(e.NewTextValue.Where(char.IsDigit).ToArray());
+
+        if (e.NewTextValue != digitsOnly)
+        {
+            entry.Text = digitsOnly;
+        }
+        entry.TextColor = digitsOnly.Length == 10 ? Colors.Green : Colors.Red;
+    }
+
+    private void TapGestureRecognizer_OnTapped(object? sender, TappedEventArgs e)
+    {
+        AgreementCheckBox.IsChecked = !AgreementCheckBox.IsChecked;
+    }
+}
